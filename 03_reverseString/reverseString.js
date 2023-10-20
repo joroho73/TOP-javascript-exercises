d62894767cbb9b8dc717f749eeadfae4f7a716ee
joroho73 <joroho73@gmail.com>
2023-10-20 15:55:47 +0100

    completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const len = string.length;
+    let reversed = "";
 
+    for (let i = len - 1; i >= 0; i--){
+        //console.log(i+string[i]);
+        reversed += string[i];
+    }
+//console.log(reversed);
+ return reversed;
 };
+
+reverseString('abcd');
 
 // Do not edit below this line
 module.exports = reverseString;
