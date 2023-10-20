908836cbdfad3f6fb2116ac8516902921f43e237
joroho73 <joroho73@gmail.com>
2023-10-20 16:51:22 +0100

    added solution to 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    //if (typeof(arr) != Object) {return "Error - not an array";}
 
+    console.log(arr);
+    console.log(args);
+
+    // for (let j = 0; j <= args.length-1; j++){
+    //     console.log(args[j])
+    // }
+
+    for (arg of args){
+        //console.log(arg);
+        for (let i = 0; i < arr.length; i++){
+            if (arg === arr[i]){
+                arr.splice(i,1);
+                //console.log(arr);
+            }
+        }
+    }
+    return arr;
 };
 
-// Do not edit below this line
+
+removeFromArray(["a",2,3],2,3,4,5)
+
 module.exports = removeFromArray;
