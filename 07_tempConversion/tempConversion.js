aae9ddaaa9a17e6511abf85cb7102848c7c2f58c
joroho73 <joroho73@gmail.com>
2023-10-21 16:28:02 +0100

    added solution to 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let cel = 0.0;
+  cel = (far - 32)*(5/9);
+  cel = cel.toFixed(1)
+  console.log(cel);
+  return parseFloat(cel);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let far = 0.0
+  far = (cel*(9/5)+32);
+far = far.toFixed(1);
+  console.log(far);
+  return parseFloat(far);
 };
 
 // Do not edit below this line
@@ -9,3 +19,6 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+convertToCelsius(100);
+convertToFahrenheit(100);
