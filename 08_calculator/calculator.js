c342e079853229c9ce2658485fa9d6a89bd313c8
joroho73 <joroho73@gmail.com>
2023-10-21 18:41:58 +0100

    added solution for 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,46 @@
-const add = function() {
+const add = function(p1,p2) {
+  return parseInt(p1) + parseInt(p2);
 	
 };
 
-const subtract = function() {
+const subtract = function(p1,p2) {
+	return parseInt(p1) - parseInt(p2);
+};
+
+const sum = function(arr) {
+  let total = 0;
+	for(let i = 0; i < arr.length; i++){
+    total += arr[i];
+  }
+  return total;
+};
+
+const multiply = function(arr) {
+  let total = 1;
+  for(let i = 0; i < arr.length; i++){
+    //console.log(arr[i]);
+    total *= parseInt(arr[i]);
+  }
+  return total;
+};
+
+const power = function(p1,p2) {
+  return parseInt(p1) ** parseInt(p2);
 	
 };
 
-const sum = function() {
-	
+const factorial = function(fact) {
+  if(fact === 0) return 1;
+
+  let res = fact;
+	for(let i = fact - 1; i >= 1; i--){
+    res *= i;
+    //console.log(res);
+  }
+  return res
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+factorial(4)
 
 // Do not edit below this line
 module.exports = {
