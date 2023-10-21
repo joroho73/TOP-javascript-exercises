7b29234cc40ea3f5b9bbb516b8cddf4a47f0a43a
joroho73 <joroho73@gmail.com>
2023-10-21 08:09:38 +0100

    added solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    // catch negative numbers
+    if(from < 0 || to < 0) return "ERROR";
 
+    // catch non-numbers
+    if(typeof(from) != "number" || typeof(to) != "number") return "ERROR";
+
+    if(to < from){
+        // An alternative way to swap the values of min and max like above is to use the array destructuring syntax.
+        // Here's an optional article on it: https://www.freecodecamp.org/news/array-destructuring-in-es6-30e398f21d10/
+        // if (min > max) [min, max] = [max, min];
+        const tempTo = to;
+        to = from;
+        from = tempTo;
+    }
+    let sum = 0;
+    for(let i = from; i <= to ; i++){
+        sum += i;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
