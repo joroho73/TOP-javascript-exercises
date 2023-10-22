7a64dab7cd9dc603aadcad7d90ba3c3f391216f5
joroho73 <joroho73@gmail.com>
2023-10-22 07:54:50 +0100

    added solution to 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if(num < 0) return "OOPS";
 
+    let fsum;
+    let f1 = 1;
+    let f2 = 0;
+
+    for(let i = 0; i < num; i++){
+        fsum = f1 + f2;
+        f1 = f2;
+        f2 = fsum;
+    }
+    console.log(fsum);
+    return fsum;
 };
+
+//fibonacci();
 
 // Do not edit below this line
 module.exports = fibonacci;
