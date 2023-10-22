296c4d11f99c198074770f44f2a2be983e66eb2f
joroho73 <joroho73@gmail.com>
2023-10-22 08:31:09 +0100

    added solution to 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,27 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    //console.log(typeof(books));
+    //console.log(Object.values(books)); 
+    const titles = [];
+    for(book of books){
+        //console.log(book.titles);
+        titles.push(book.title);
+    }
+    console.log(titles);
+    return titles;
 };
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
+
+  getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
